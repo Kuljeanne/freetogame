4268c306e16f51f6e9cbab4f3f8a8899f6e98f89
Zhanna Kulagina <kuljeanne@gmail.com>
2023-08-28 22:31:47 +0300

    refactor

---
+++ src/components/GameInfo/GameInfo.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Button, Carousel } from "antd";
+import { ArrowLeftOutlined, RightCircleOutlined } from "@ant-design/icons";
+import cn from "classnames";
+
+import { datePerse } from "../../utils";
+import { IGameDetails } from "../../types";
+
+import styles from "./GameInfo.module.css";
+
+interface IGameInfoProps extends IGameDetails {
+  onClick: () => void;
+}
+
+export const GameInfo = ({
+  title,
+  thumbnail,
+  game_url,
+  short_description,
+  description,
+  screenshots,
+  developer,
+  publisher,
+  release_date,
+  genre,
+  minimum_system_requirements,
+  platform,
+  onClick,
+}: IGameInfoProps) => {
+  return (
+    <div className={styles.details}>
+      <div className={styles.stickyContainer}>
+        <div className={styles.play}>
+          <img src={thumbnail} alt={title} />
+          <Button
+            style={{ boxShadow: "none" }}
+            href={game_url}
+            target="_blank"
+            type="primary"
+          >
+            PLAY NOW <RightCircleOutlined />
+          </Button>
+          <Button onClick={onClick} className={styles.btn} type="default">
+            <ArrowLeftOutlined />
+            Back to the games' list
+          </Button>
+        </div>
+      </div>
+      <div className={styles.info}>
+        <div className={styles.infoBlock}>
+          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.short}>{short_description}</p>
+        </div>
+        <div className={styles.infoBlock}>
+          <h4 className={styles.subtitle}>About {title}</h4>
+          <p className={styles.desc}>{description}</p>
+        </div>
+        <div className={styles.infoBlock}>
+          <h4 className={styles.subtitle}>{title} Screenshots</h4>
+          <Carousel autoplay>
+            {screenshots.length > 0 &&
+              screenshots.map((img) => (
+                <img key={img.id} src={img.image} alt="screenshots" />
+              ))}
+          </Carousel>
+        </div>
+        <div className={styles.infoBlock}>
+          <h4 className={styles.subtitle}>Additional Information</h4>
+          <ul className={cn(styles.addInfo, styles.list)}>
+            <li className={styles.about}>
+              <span>Title </span>
+              {title}
+            </li>
+            <li className={styles.about}>
+              <span>Developer </span>
+              {developer}
+            </li>
+            <li className={styles.about}>
+              <span>Publisher </span>
+              {publisher}
+            </li>
+            <li className={styles.about}>
+              <span>Release Date </span>
+              {datePerse(release_date)}
+            </li>
+            <li className={styles.about}>
+              <span>Genre </span>
+              {genre}
+            </li>
+            <li className={styles.about}>
+              <span>Platform </span>
+              {platform}
+            </li>
+          </ul>
+        </div>
+        <div className={styles.infoBlock}>
+          <h4 className={styles.subtitle}>
+            Minimum System Requirements <span>({platform})</span>
+          </h4>
+          <ul className={cn(styles.list, styles.system)}>
+            <li className={styles.about}>
+              <span>OS </span>
+              {minimum_system_requirements.os}
+            </li>
+            <li className={styles.about}>
+              <span>Processor </span>
+              {minimum_system_requirements.processor}
+            </li>
+            <li className={styles.about}>
+              <span>Memory </span>
+              {minimum_system_requirements.memory}
+            </li>
+            <li className={styles.about}>
+              <span>Graphics </span>
+              {datePerse(minimum_system_requirements.graphics)}
+            </li>
+            <li className={styles.about}>
+              Storage <span>{minimum_system_requirements.storage}</span>
+            </li>
+            <li className={styles.about}>
+              Additional notes{" "}
+              <span>Specifications may change during development</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/routes/GameDetails/GameDetails.tsx
+++ src/routes/GameDetails/GameDetails.tsx
@@ -1,16 +1,29 @@
 import React, { useEffect } from "react";
 import cn from "classnames";
-import { Button, Carousel } from "antd";
-import { ArrowLeftOutlined, RightCircleOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 
-import { datePerse } from "../../utils";
 import Footer from "../../components/Footer";
+import GameInfo from "../../components/GameInfo";
 
 import styles from "./GameDetails.module.css";
 import { GAME } from "./mockdata";
 
 export const GameDetails = () => {
+  const {
+    id,
+    title,
+    thumbnail,
+    game_url,
+    short_description,
+    description,
+    screenshots,
+    developer,
+    publisher,
+    release_date,
+    genre,
+    minimum_system_requirements,
+    platform,
+  } = GAME;
   const navigate = useNavigate();
 
   const handleBackBtn = () => {
@@ -24,108 +37,22 @@ export const GameDetails = () => {
   return (
     <>
       <div className={cn(styles.container, "wrapper")}>
-        <div className={styles.details}>
-          <div className={styles.stickyContainer}>
-            <div className={styles.play}>
-              <img src={GAME.thumbnail} alt={GAME.title} />
-              <Button
-                style={{ boxShadow: "none" }}
-                href={GAME.game_url}
-                target="_blank"
-                type="primary"
-              >
-                PLAY NOW <RightCircleOutlined />
-              </Button>
-              <Button
-                onClick={handleBackBtn}
-                className={styles.btn}
-                type="default"
-              >
-                <ArrowLeftOutlined />
-                Back to the games' list
-              </Button>
-            </div>
-          </div>
-          <div className={styles.info}>
-            <div className={styles.infoBlock}>
-              <h1 className={styles.title}>{GAME.title}</h1>
-              <p className={styles.short}>{GAME.short_description}</p>
-            </div>
-            <div className={styles.infoBlock}>
-              <h4 className={styles.subtitle}>About {GAME.title}</h4>
-              <p className={styles.desc}>{GAME.description}</p>
-            </div>
-            <div className={styles.infoBlock}>
-              <h4 className={styles.subtitle}>{GAME.title} Screenshots</h4>
-              <Carousel autoplay>
-                {GAME.screenshots.length > 0 &&
-                  GAME.screenshots.map((img) => (
-                    <img key={img.id} src={img.image} alt="screenshots" />
-                  ))}
-              </Carousel>
-            </div>
-            <div className={styles.infoBlock}>
-              <h4 className={styles.subtitle}>Additional Information</h4>
-              <ul className={cn(styles.addInfo, styles.list)}>
-                <li className={styles.about}>
-                  <span>Title </span>
-                  {GAME.title}
-                </li>
-                <li className={styles.about}>
-                  <span>Developer </span>
-                  {GAME.developer}
-                </li>
-                <li className={styles.about}>
-                  <span>Publisher </span>
-                  {GAME.publisher}
-                </li>
-                <li className={styles.about}>
-                  <span>Release Date </span>
-                  {datePerse(GAME.release_date)}
-                </li>
-                <li className={styles.about}>
-                  <span>Genre </span>
-                  {GAME.genre}
-                </li>
-                <li className={styles.about}>
-                  <span>Platform </span>
-                  {GAME.platform}
-                </li>
-              </ul>
-            </div>
-            <div className={styles.infoBlock}>
-              <h4 className={styles.subtitle}>
-                Minimum System Requirements <span>({GAME.platform})</span>
-              </h4>
-              <ul className={cn(styles.list, styles.system)}>
-                <li className={styles.about}>
-                  <span>OS </span>
-                  {GAME.minimum_system_requirements.os}
-                </li>
-                <li className={styles.about}>
-                  <span>Processor </span>
-                  {GAME.minimum_system_requirements.processor}
-                </li>
-                <li className={styles.about}>
-                  <span>Memory </span>
-                  {GAME.minimum_system_requirements.memory}
-                </li>
-                <li className={styles.about}>
-                  <span>Graphics </span>
-                  {datePerse(GAME.minimum_system_requirements.graphics)}
-                </li>
-                <li className={styles.about}>
-                  Storage{" "}
-                  <span>{GAME.minimum_system_requirements.storage}</span>
-                </li>
-                <li className={styles.about}>
-                  Additional notes{" "}
-                  <span>Specifications may change during development</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        <GameInfo
+          id={id}
+          title={title}
+          thumbnail={thumbnail}
+          game_url={game_url}
+          short_description={short_description}
+          description={description}
+          screenshots={screenshots}
+          developer={developer}
+          publisher={publisher}
+          release_date={release_date}
+          genre={genre}
+          minimum_system_requirements={minimum_system_requirements}
+          platform={platform}
+          onClick={handleBackBtn}
+        />
       </div>
       <Footer />
     </>
