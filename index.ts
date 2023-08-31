dbaa13cd2d9c7fb8c57aff03448e172d68c66be0
Zhanna Kulagina <kuljeanne@gmail.com>
2023-08-31 18:20:32 +0300

    feat: add cors

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
-import express, {Express, Request, Response } from "express";
+import express, { Express, Request, Response } from "express";
 import router from "./routes/games.router";
 import dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app: Express = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+  })
+);
 
 app.get("/ping", (req: Request, res: Response) => {
   res.send("Pong");
